11d0a828d3339b6ac9d32e2452a6425ecc54d7e0
rgsk <guptarahul70322@gmail.com>
2022-11-10 21:59:55 +0530

    tailwind setup

--- src/components/First.tsx
+++ src/components/First.tsx
@@ -1,5 +1,7 @@
 interface IFirstProps {}
 const First: React.FC<IFirstProps> = ({}) => {
-  return <div>First</div>;
+  return (
+    <h1 className="text-3xl font-bold underline text-red-400">Hello world!</h1>
+  );
 };
 export default First;
---
+++ src/lib/index.tsx
@@ -0,0 +1,1 @@
+export { default as First } from "src/components/First";
